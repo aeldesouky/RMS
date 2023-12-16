745aae738d892cbca50f5c453fbf9c193ba8aadd
MikeBlak <blackxkingxdragon@gmail.com>
2023-12-16 17:02:59 +0200

    Room Schedule Fix

--- Pages/Schedule.cshtml.cs
+++ Pages/Schedule.cshtml.cs
@@ -11,17 +11,34 @@ public class Schedule : PageModel
     
     [BindProperty(SupportsGet = true)]
     public string GP { get; set; }
+    
+    [BindProperty(SupportsGet = true)]
+    public string RoomName { get; set; }
 
     public void OnGet()
     {
-        if (string.IsNullOrEmpty(GP))
+        if (!string.IsNullOrEmpty(GP))
         {
             string conString = @"Data Source=DESKTOP-R0BEJSG;Initial Catalog=RMS_DB;Integrated Security=True";
             SqlConnection con = new SqlConnection(conString);
-            string queryString = "SELECT GP, CourseCode, RoomName, Day, LecTime FROM Schedule";
+            string queryString = "SELECT GP, CourseCode, RoomName, Day, LecTime FROM Schedule WHERE GP = @GP";
 
             SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@GP", GP);
+            con.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            ScheduleTable = new DataTable();
+            ScheduleTable.Load(reader);
+            
+        }
+        else if (!string.IsNullOrEmpty(RoomName))
+        {
+            string conString = @"Data Source=DESKTOP-R0BEJSG;Initial Catalog=RMS_DB;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            string queryString = "SELECT GP, CourseCode, RoomName, Day, LecTime FROM Schedule WHERE RoomName = @RoomName";
 
+            SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@RoomName", RoomName);
             con.Open();
             SqlDataReader reader = command.ExecuteReader();
             ScheduleTable = new DataTable();
@@ -31,10 +48,10 @@ public class Schedule : PageModel
         {
             string conString = @"Data Source=DESKTOP-R0BEJSG;Initial Catalog=RMS_DB;Integrated Security=True";
             SqlConnection con = new SqlConnection(conString);
-            string queryString = "SELECT GP, CourseCode, RoomName, Day, LecTime FROM Schedule WHERE GP = @GP";
+            string queryString = "SELECT GP, CourseCode, RoomName, Day, LecTime FROM Schedule";
 
             SqlCommand command = new SqlCommand(queryString, con);
-            command.Parameters.AddWithValue("@GP", GP);
+
             con.Open();
             SqlDataReader reader = command.ExecuteReader();
             ScheduleTable = new DataTable();
